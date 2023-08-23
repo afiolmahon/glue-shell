d611a08f8038c65c1d02d960dbc5188872e0f10c
Antonio Fiol-Mahon <antonio@veobot.com>
2023-08-23 09:52:23 -0400

    lib/command.hpp: reduce header includes

--- src/lib/command.cpp
+++ src/lib/command.cpp
@@ -1,6 +1,29 @@
 #include "include/command.hpp"
 
+#include <cstring>
+
+#include <pty.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 namespace crew {
+namespace {
+/** Wait for a child process to exit and return its exit code */
+int childExit(int pid)
+{
+    int status{};
+    if (::waitpid(pid, &status, 0) == -1) {
+        fatal("waitpid failed: ", std::strerror(errno));
+    }
+
+    if (!WIFEXITED(status)) {
+        fatal("child failed to exit normally");
+    }
+
+    return WEXITSTATUS(status);
+}
+} // namespace
 
 int Command::run(std::ostream& outStr, std::ostream& errStr) const
 {
@@ -96,4 +119,71 @@ int Command::run(std::ostream& outStr, std::ostream& errStr) const
 
     return childExit(pid);
 }
+
+int Command::runPty(std::ostream& outStr) const
+{
+    if (m_verbose) {
+        describe();
+    }
+    int amaster{};
+
+    int pid = ::forkpty(&amaster, /*name*/ nullptr, nullptr, nullptr);
+
+    if (pid == -1) { // error
+        fatal("fork() failed");
+    }
+    if (pid == 0) { // child
+        if (m_cd.has_value()) {
+            current_path(*m_cd);
+        }
+
+        // setup subprocess specific environment variables
+        for (const auto& [k, v] : m_envOverride) {
+            if (::setenv(k.c_str(), v.c_str(), 1) == -1) {
+                fatal("failed to update environment variable ", k, ": ", std::strerror(errno));
+            }
+        }
+
+        std::string command = m_command;
+        auto args = m_args;
+
+        std::vector<char*> argv{command.data()};
+        for (auto& a : args) {
+            argv.push_back(a.data());
+        }
+        argv.push_back(nullptr);
+
+        if (::execvp(m_command.c_str(), argv.data()) == -1) {
+            fatal("execvp failed: ", std::strerror(errno));
+        }
+    }
+
+    // parent
+    char buffer[4096];
+
+    // TODO: support the input side of the psuedoterminal
+    // try this approach: https://rmathew.blogspot.com/2006/09/terminal-sickness.html
+
+    while (1) {
+        ssize_t count = ::read(amaster, buffer, sizeof(buffer));
+        if (count == -1) {
+            if (errno == EINTR) {
+                continue;
+            } else if (errno == EIO) { // child side closed
+                break;
+            } else {
+                fatal("read() failed: ", std::strerror(errno));
+            }
+        } else if (count == 0) {
+            break;
+        } else {
+            outStr << std::string_view(buffer, count);
+        }
+    }
+    ::close(amaster);
+    outStr.flush();
+
+    return childExit(pid);
+}
+
 } // namespace crew
--- src/lib/include/command.hpp
+++ src/lib/include/command.hpp
@@ -6,7 +6,6 @@
 
 #include "util.hpp"
 
-#include <cstring>
 #include <filesystem>
 #include <iostream>
 #include <map>
@@ -14,27 +13,7 @@
 #include <string>
 #include <vector>
 
-#include <pty.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-
 namespace crew {
-
-/** Wait for a child process to exit and return its exit code */
-inline int childExit(int pid)
-{
-    int status{};
-    if (::waitpid(pid, &status, 0) == -1) {
-        fatal("waitpid failed: ", std::strerror(errno));
-    }
-
-    if (!WIFEXITED(status)) {
-        fatal("child failed to exit normally");
-    }
-
-    return WEXITSTATUS(status);
-}
 
 class Command {
 public:
@@ -123,73 +102,8 @@ public:
         return std::move(*this);
     }
 
-    int runPty(std::ostream& outStr = std::cout) const
-    {
-        if (m_verbose) {
-            describe();
-        }
-        int amaster{};
-
-        int pid = ::forkpty(&amaster, /*name*/ nullptr, nullptr, nullptr);
-
-        if (pid == -1) { // error
-            fatal("fork() failed");
-        }
-        if (pid == 0) { // child
-            if (m_cd.has_value()) {
-                current_path(*m_cd);
-            }
-
-            // setup subprocess specific environment variables
-            for (const auto& [k, v] : m_envOverride) {
-                if (::setenv(k.c_str(), v.c_str(), 1) == -1) {
-                    fatal("failed to update environment variable ", k, ": ", std::strerror(errno));
-                }
-            }
-
-            std::string command = m_command;
-            auto args = m_args;
-
-            std::vector<char*> argv{command.data()};
-            for (auto& a : args) {
-                argv.push_back(a.data());
-            }
-            argv.push_back(nullptr);
-
-            if (::execvp(m_command.c_str(), argv.data()) == -1) {
-                fatal("execvp failed: ", std::strerror(errno));
-            }
-        }
-
-        // parent
-        char buffer[4096];
-
-        // TODO: support the input side of the psuedoterminal
-        // try this approach: https://rmathew.blogspot.com/2006/09/terminal-sickness.html
-
-        while (1) {
-            ssize_t count = ::read(amaster, buffer, sizeof(buffer));
-            if (count == -1) {
-                if (errno == EINTR) {
-                    continue;
-                } else if (errno == EIO) { // child side closed
-                    break;
-                } else {
-                    fatal("read() failed: ", std::strerror(errno));
-                }
-            } else if (count == 0) {
-                break;
-            } else {
-                outStr << std::string_view(buffer, count);
-            }
-        }
-        ::close(amaster);
-        outStr.flush();
-
-        return childExit(pid);
-    }
-
     int run(std::ostream& outStr = std::cout, std::ostream& errStr = std::cerr) const;
+    int runPty(std::ostream& outStr = std::cout) const;
 
     void tryRun(std::ostream& outStr = std::cout, std::ostream& errStr = std::cerr) const
     {
